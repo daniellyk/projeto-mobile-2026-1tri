b5471bd1802b8d41a96d8c205833b9fbd95ef40d
santoswaitherzx <christopher.whaither.santos@escola.pr.gov.br>
2026-03-26 09:16:27 -0300

    Adcionei cor ao Background, tenho que colocar as informações dos médicos.

--- mobile/app/(tabs)/listagem-doutores.tsx
+++ mobile/app/(tabs)/listagem-doutores.tsx
@@ -5,9 +5,14 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 const ViewBoxesWithColorAndText = () => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flexDirection: 'row'}}>
-        <View style={{height: 100, backgroundColor: '#203298', flex: 20}} />
-        <Text>Hello World!</Text>
+      {/* Adicione flex: 1 aqui para a SafeAreaView ocupar a tela toda */}
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#203298' }}>
+        
+        {/* Remova o height fixo e use flex: 1 para preencher o espaço */}
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ color: 'white' }}>Dr.Carlos Matheus</Text>
+        </View>
+        
       </SafeAreaView>
     </SafeAreaProvider>
   );
