06f27bfec3cafdc45f28fcbf4f6c454fe174474b
gusta740 <gustavo.kukla@escola.pr.gov.br>
2026-04-06 12:06:01 -0300

    Modificando icones

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -5,6 +5,10 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome7 from '@expo/vector-icons/FontAwesome';
+import FontAwesome8 from '@expo/vector-icons/FontAwesome6';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -24,12 +28,41 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      name="explore"
+     options={{
+      title: 'Armazém', // Mudei o título também, caso queira
+      tabBarIcon: ({ color }) => (
+      <FontAwesome5 name="warehouse" size={24} color={color} />
+    ),
+  }}
+/>
+<Tabs.Screen
+  name="telafinal" // ou o nome que preferir para essa rota
+  options={{
+    title: 'Check-in', // Título que aparece abaixo do ícone
+    tabBarIcon: ({ color }) => (
+      <FontAwesome6 name="calendar-check" size={24} color={color} />
+    ),
+  }}
+/>
+<Tabs.Screen
+  name="agendamento" // Nome da rota
+  options={{
+    title: 'Agenda', // Título que aparece na aba
+    tabBarIcon: ({ color }) => (
+      <FontAwesome5 name="calendar-day" size={24} color={color} />
+    ),
+  }}
+/>
+<Tabs.Screen
+  name="listagem-doutores" // Nome da rota
+  options={{
+    title: 'lista', // Título que aparece na aba
+    tabBarIcon: ({ color }) => (
+      <FontAwesome6 name="rectangle-list" size={24} color={color}  />
+    ),
+  }}
+/>
     </Tabs>
   );
 }
