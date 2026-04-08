b07f9d081313087d45a2233fa13d6f0bb6704bbb
daniellyk <danielly.grando@escola.pr.gov.br>
2026-04-08 07:31:47 -0300

    adicionando a interface principal da aplicação

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -1,112 +1,99 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
 
-export default function TabTwoScreen() {
+const ViewBoxesWithColorAndText = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
-}
+    <SafeAreaProvider>
+      {/*fundo da aplicação*/}
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#2D43A6' }}>
+       
+        {/* card da interface */}
+        <View style={styles.mainCard}>
+         
+          {/* header */}
+          <View style={{ marginBottom: 30 }}>
+            <View style={{ width: 30, height: 30, backgroundColor: '#FFFFFF33', borderRadius: 5 }} /> {/* Simulando Ícone Menu */}
+            <Text style={styles.title}>Bom Dia, Fulano{"\n"}</Text>
+            <Text style={styles.subtitle}>....dnweibfiwbgbi</Text>
+          </View>
 
+
+          <View style={styles.divider} />
+
+
+          <Text style={styles.sectionTitle}>Insira qual consulta deseja realizar:</Text>
+
+
+          {/* linha */}
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+            {/* círculo do ícone*/}
+            <View style={{ height: 60, width: 60, backgroundColor: 'white', borderRadius: 30, flex: 0.2 }} />
+           
+            {/* blocos do texto onde vai ficar as funções*/}
+            <View style={{ flex: 0.7, paddingHorizontal: 15 }}>
+              <View style={{ height: 15, backgroundColor: 'white', width: '80%', marginBottom: 8 }} />
+              <View style={{ height: 10, backgroundColor: '#FFFFFF66', width: '100%' }} />
+            </View>
+
+
+            {/* círculo do lado para colocar a seta */}
+            <View style={{ height: 25, width: 25, backgroundColor: '#FFFFFF33', borderRadius: 15, flex: 0.1 }} />
+          </View>
+
+
+         
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+            <View style={{ height: 60, width: 60, backgroundColor: 'white', borderRadius: 30, flex: 0.2 }} />
+            <View style={{ flex: 0.7, paddingHorizontal: 15 }}>
+              <View style={{ height: 15, backgroundColor: 'white', width: '60%', marginBottom: 8 }} />
+              <View style={{ height: 10, backgroundColor: '#FFFFFF66', width: '90%' }} />
+            </View>
+            <View style={{ height: 25, width: 25, backgroundColor: '#FFFFFF33', borderRadius: 15, flex: 0.1 }} />
+          </View>
+
+
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+ {/* CSS do react */}
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  mainCard: {
+    flex: 1,
+    backgroundColor: '#2D43A6',
+    marginTop: 40,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30,
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  title: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 20,
   },
+  subtitle: {
+    color: '#B0C4DE',
+    marginTop: 10,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#FFFFFF33',
+    marginVertical: 25,
+  },
+  sectionTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 25,
+  }
 });
+
+
+export default ViewBoxesWithColorAndText;
+
+
