e267cd2070d074e5cbd66838ddc25d88c19f5b09
dudabastos1909 <dudabastos21203@.com>
2026-04-16 11:11:39 -0300

    adicionei textos e imagens

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,55 +1,92 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import { Platform } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-
-
-
-const login = () => {
+export default function HomeScreen() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-      <Image  
+    <LinearGradient
+      colors={["#0b1c3a", "#2a3b7a"]}
+      style={styles.container}
+    >
+      <Image
+        source={require("../../assets/images/image.png")}
+        style={styles.image}
+      />
 
- 
-/>
-   
-        <View style={styles.fullScreenBlue}>
-          <Text style={styles.text}>MedClinic</Text>
-       
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      <Text style={styles.title}>MedClinic</Text>
+
+      <View style={styles.card}>
+        <Image
+          source={require("../../assets/images/doctor.png")}
+          style={styles.image}
+          
+        />
+      </View>
+
+      <Text style={styles.description}>
+        Agendamento de exames e consultas simples, rápido e fácil.
+      </Text>
+      <Text style={styles.description}>
+        Acesse sua conta e agende já!
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>entrar</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+
+    
   );
-};
-
-
-
+}
 
 const styles = StyleSheet.create({
-  imagemlocal: {
-
-
-
-
-  },
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
   },
-  fullScreenBlue: {
-      flex: 1,
-      backgroundColor: '#203298',
-      padding: 20,
+  Image: {
+    width: 60,
+    height: 60,
+    marginBottom: 10,
   },
-  text: {
-    fontSize: 26,
+  title: {
+    fontSize: 30,
     color: 'white',
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
-  }
+
+  },
+  card: {
+    width: "90%",
+    height: 180,
+    backgroundColor: "#eee",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  description: {
+    color: "#fff",
+    textAlign: "center",
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  button: {
+    marginTop: 30,
+    backgroundColor: "#d9d9d9",
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 25,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#000",
+  },
 });
-
-
-export default login;
